ef2b6e2148b69031e9d85bb6feda6879cd68859d
Vinayak <vinayakpawarinstance@gmail.com>
2024-12-20 10:07:24 +0530

    display main menu items using ngFor Directive

--- src/app/header/main-menu/main-menu.component.ts
+++ src/app/header/main-menu/main-menu.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-menu.component.css']
 })
 export class MainMenuComponent {
+    mainMenuItems:string[] = ["Home","Product","Sale","New Arrival","Contact"];
 
 }
